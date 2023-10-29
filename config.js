cad9b34a9fe4a0baa23892143d8fff5a9db13d1e
Tuấn Kiệt Nguyễn <ntkiet11082002@mail.com>
2023-10-30 01:57:52 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,44 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Happy birthday to my bestie",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Chúc Thùy Trang yêu sinh nhật hạnh phúc nhaaaa,",  // 同上...
+        "sinh nhật lần đầu xa nhau nên nhớ quãi ò ",
+        "Chúc kưng mãi trở nên xinh đẹp tự tin, hạnh phúc,",
+        "chúc em tìm được real luv nhaaaa bỏ anh hutech đi nhóa",
+        "sớm mọc lại chân mày  bớt khờ lại nha má",
+        "để khỏi bị lừa tình lừa tiền nhoaaa呢",
+        "Không cần phải thay đổi bản thân gì hết",
+        "cứ mãi là chính mình là đáng yêu nhất rồi, ",
+        "không phải giảm cân không phải tự ti",
+        "Mãi mụt tình bạn mãi ",
+        "mận mãi keoooo,",
+        "_v.meili~~",
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Bắt đầu",
+        play: "Music",
+        bannar_coming: "Next",
+        balloons_flying: "Có vẻ như thiếu một cái gì đó",
+        cake_fadein: "Bánh kem?",
+        light_candle: "Nến?",
+        wish_message: "Happy birthday,bestie",
+        story: "Có vài lời muốn nói là:",
+    }
+};
